bd2973d300b5dd0e5d1d7cc1a054c58d0b30779d
SukaSama <suenny.s@gmail.com>
2021-02-24 17:17:51 -0300

    Inclusão do contexto da aplicação. Aula 03 do nlw#04.

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,9 +1,16 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/Countdown.module.css';
 
+
+let countdownTimeout : NodeJS.Timeout;
+
 export function Countdown(){
-    const [time, setTime] = useState(25 * 60); //25 minutos em segundos
-    const [active,setActive] = useState(false);
+    const { startNewChallenge } = useContext(ChallengesContext);
+
+    const [time, setTime] = useState(0.1 * 60); //25 minutos em segundos
+    const [isActive,setisActive] = useState(false);
+    const [hasFinished, setHasFinished] = useState(false);
 
     const minutes = Math.floor(time/60);
     const seconds = time % 60;
@@ -12,16 +19,24 @@ export function Countdown(){
     const [secondLeft, secondRight] = String(seconds).padStart(2,'0').split('');// o padStart analisa se existem dois caracteres, se não, ele preenche o começo da string com o segundo argumento ,'0' no caso, 
     
     function startCountdown() {
-        setActive(true);
+        setisActive(true);
     }
-
+    function resetCountdown(){        
+        clearTimeout(countdownTimeout); //Impedindo o setTimeout de funcionar
+        setisActive(false); 
+        setTime(0.1 * 60);
+    }
     useEffect(() =>{ //useEffect executa algo (dentro de sua arrow function passada como primeiro parâmetro) quando algo muda (algo passado como segundo parâmetro, no caso o active e o time)
-        if(active && time>0){ //setTimeout diz que algo vai ser executado depois de um tempo, no caso, a arrow function passada como primeiro argumento e o tempo é um segundo (1000) passado como segundo argumento
-            setTimeout(() =>{
+        if(isActive && time>0){ //setTimeout diz que algo vai ser executado depois de um tempo, no caso, a arrow function passada como primeiro argumento e o tempo é um segundo (1000) passado como segundo argumento
+            countdownTimeout = setTimeout(() =>{
                 setTime(time-1);
             }, 1000);
+        }else if(isActive && time === 0){
+            setHasFinished(true);
+            setisActive(false);
+            startNewChallenge();
         }
-    }, [active, time]);//quando o active muda e quando o time muda ele chama a arrow function e assim o setTimeOut é chamado e o time muda novamente e o ciclo se mantem até o time zerar
+    }, [isActive, time]);//quando o active muda e quando o time muda ele chama a arrow function e assim o setTimeOut é chamado e o time muda novamente e o ciclo se mantem até o time zerar
     
     return (
         <div>
@@ -36,7 +51,26 @@ export function Countdown(){
                     <span>{secondRight}</span>
                 </div>
             </div>
-            <button type="button" className={styles.countdownButton} onClick={startCountdown}>Iniciar um ciclo</button>
+            {hasFinished ? (
+                <button disabled className={styles.countdownButton}>
+                    Ciclo encerrado
+                </button>
+            ):(
+                <> 
+                    { isActive ? (
+                    <button type="button" className={`${styles.countdownButton} ${styles.countdownButtonActive}`} onClick={resetCountdown}>
+                        Abandonar ciclo
+                    </button>
+                    ):(
+                    <button type="button" className={styles.countdownButton} onClick={startCountdown}>
+                        Iniciar um ciclo
+                    </button>
+                    )}
+                </>
+            )}
+            
+            
+            
         </div>
     );
 }
